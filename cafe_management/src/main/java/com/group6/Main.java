cb197a4264b7393267a131ea2251db7411cf3bb1
mcdonaldlk <lance.k.mcdonald@gmail.com>
2026-03-18 20:51:18 -0500

    Initialized maven project

---
+++ cafe_management/src/main/java/com/group6/Main.java
@@ -0,0 +1,13 @@
+package com.group6;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
